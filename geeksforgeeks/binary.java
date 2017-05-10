b1bcb40b39bef3d2393fc49839c2d582c39b3cb9
Aditya Verma <adityasinghverma314@gmail.com>
2017-05-10 14:01:19 +0530

    Update binary.java
    solution for http://practice.geeksforgeeks.org/problems/binary-number-to-decimal-number/0

--- geeksforgeeks/binary.java
+++ geeksforgeeks/binary.java
@@ -1,13 +1,7 @@
-// http://practice.geeksforgeeks.org/problems/binary-number-to-decimal-number/0
-
-
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
 class GFG {
-
          public static void main (String [] args)  {
              Scanner ac = new Scanner(System.in);
              int k = ac.nextInt();
@@ -16,8 +10,11 @@ class GFG {
                  int len = binu.length();
                  long answer = 0;
                  for ( int i = 0; i < len; i++ ) {
-                     if ( binu.charA)
+                     if ( binu.charAt(i) == '1' ) {
+                         answer += Math.pow(2, len-i-1);
+                     }
                  }
+                 System.out.println(answer);
              }
 	 
 	 }
