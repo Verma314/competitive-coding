ce3bdf956e493746a7f41cd86d657bb8cbba05ce
Aditya Verma <adityasinghverma314@gmail.com>
2017-05-10 13:57:30 +0530

    Create binary.java

---
+++ geeksforgeeks/binary.java
@@ -0,0 +1,24 @@
+// http://practice.geeksforgeeks.org/problems/binary-number-to-decimal-number/0
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+
+         public static void main (String [] args)  {
+             Scanner ac = new Scanner(System.in);
+             int k = ac.nextInt();
+             while ( k -- > 0 ) {
+                 String binu = ac.next();
+                 int len = binu.length();
+                 long answer = 0;
+                 for ( int i = 0; i < len; i++ ) {
+                     if ( binu.charA)
+                 }
+             }
+	 
+	 }
+}
