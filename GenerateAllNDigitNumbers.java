59d6acec3cff23b83fe087b26d384f0441c65141
Aditya Verma <adityasinghverma314@gmail.com>
2017-07-04 12:48:33 +0530

    IMP.

---
+++ GenerateAllNDigitNumbers.java
@@ -0,0 +1,40 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class GenerateAllNDigitNumbers {
+    
+    static void printRecur(String ans, int start, int end) {
+        
+        if ( start == end ) {
+            System.out.print(ans+" ");
+            return;
+        }
+        
+    
+        if  ( start == 0 ){
+            for ( int i = 1; i <= 9 ; i++ ) 
+                printRecur ( ans+Integer.toString(i), 1, end);
+        }
+        
+        else {
+            for ( int i = 0; i <= 9 ; i++ ) 
+                printRecur ( ans+Integer.toString(i), start+1, end );
+        }
+        
+    }
+
+
+
+
+
+    public static void main(String [] args ) {
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        while ( t-- > 0) {
+            int n  = sc.nextInt();
+            String ans = "";
+            printRecur(ans,0,n);
+        }
+    }
+}
